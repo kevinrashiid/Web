decb13b97b9006762cfb1167cb1c7544d036c3ee
kevin <kevin@PanDora>
2023-02-02 19:52:30 +0100

    subido desde clase

---
+++ _11_ejemplo_BD/src/Principal/InserccionContacto.java
@@ -0,0 +1,12 @@
+package Principal;
+
+public class InserccionContacto {
+
+	public static void main(String[] args) {
+
+		// paso 1: Establecer conexión a la bbdd 
+		String cadenaCon = "jdbc:mysql://localhost:3306/agenda";
+		String user="root";
+		String pwd= "root";
+	}
+}
