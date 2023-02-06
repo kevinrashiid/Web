6c95533b49558febc46458685c7b4c943e76d96f
kevin <kevin@AsusTUF>
2023-02-06 10:40:17 +0100

    subido desde clase

---
+++ _14_registro_contactos/src/main/java/service/ContactosService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.Contacto;
+
+public interface ContactosService {
+
+	void agregarContacto(Contacto contacto);
+	void eliminarContacto(String nombre);
+	void modificarEmail(String nombre, String nuevoEmail);
+
+}
---
+++ _14_registro_contactos/src/main/java/service/ContactosServiceImpl.java
@@ -0,0 +1,57 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Contacto;
+import service.locator.ConnectionLocator;
+
+public class ContactosServiceImpl implements ContactosService {	
+	@Override
+	public void agregarContacto(Contacto contacto) {
+		try(Connection con=ConnectionLocator.getConnection();){
+			String sql="insert into contactos(nombre,email,edad) values(?,?,?)";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, contacto.getNombre());
+			ps.setString(2, contacto.getEmail());
+			ps.setInt(3, contacto.getEdad());
+			ps.execute();
+		}
+		catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Override
+	public void eliminarContacto(String nombre) {
+		try(Connection con=ConnectionLocator.getConnection();){
+			//Establecer conexión con base de datos
+			String sql="delete from contactos where nombre=?";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, nombre);
+			ps.execute();
+		}
+		catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void modificarEmail(String nombre, String nuevoEmail) {
+		try(Connection con=ConnectionLocator.getConnection();){
+			//Establecer conexión con base de datos
+			String sql="update contactos set email=? where nombre=?";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, nuevoEmail);
+			ps.setString(2, nombre);
+			ps.execute();
+		}
+		catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+}
